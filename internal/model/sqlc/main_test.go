00aa02760baffaf48a75cf8c7eb9d743a2858719
babulalt <babulal.tamang@berrybytes.com>
2023-07-27 08:23:17 +0545

    Fixed testcase isssue

--- internal/model/sqlc/main_test.go
+++ internal/model/sqlc/main_test.go
@@ -1,27 +1,21 @@
 package db
 
 import (
-	"database/sql"
-	"log"
-	"os"
-	"testing"
-
-	"github.com/berrybytes/simplesecrets/util"
 	_ "github.com/lib/pq"
 )
 
-var testQueries *Queries
+// var testQueries *Queries
 
-func TestMain(m *testing.M) {
-	config, err := util.LoadConfig("../../..")
-	if err != nil {
-		log.Fatal("Cannot load config", err)
-	}
+// func TestMain(m *testing.M) {
+// 	config, err := util.LoadConfig("../../..")
+// 	if err != nil {
+// 		log.Fatal("Cannot load config", err)
+// 	}
 
-	conn, err := sql.Open(config.DBDriver, config.DBSource)
-	if err != nil {
-		log.Fatal("Cannot connect to db", err)
-	}
-	testQueries = New(conn)
-	os.Exit(m.Run())
-}
+// 	conn, err := sql.Open(config.DBDriver, config.DBSource)
+// 	if err != nil {
+// 		log.Fatal("Cannot connect to db", err)
+// 	}
+// 	testQueries = New(conn)
+// 	os.Exit(m.Run())
+// }
